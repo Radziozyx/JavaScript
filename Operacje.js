382970af93365264ce63dba3b093ec595cb0f444
Radziozyx <lolradzio@gmail.com>
2024-09-19 12:32:08 +0200

    Create Operacje.js

---
+++ Operacje.js
@@ -0,0 +1,8 @@
+const x = parseFloat(prompt("Podaj pierwszą liczbę:"));
+const y = parseFloat(prompt("Podaj drugą liczbę:"));
+
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
