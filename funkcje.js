6814a57faaee17f49e60fa15f3cc1feff1b0bd0d
Radziozyx <lolradzio@gmail.com>
2024-09-19 12:45:50 +0200

    Create funkcje.js

---
+++ funkcje.js
@@ -0,0 +1,11 @@
+const x = parseFloat(prompt("Podaj pierwsza liczbe:"));
+const y = parseFloat(prompt("Podaj druga liczbe:"));
+
+function sum(x,y)
+{
+    return x+y;
+}
+
+const wynik=(x+y);
+
+console.log(wynik);
